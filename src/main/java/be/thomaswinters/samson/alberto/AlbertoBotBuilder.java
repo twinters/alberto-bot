9a2e977711c8685dc981888c8ebd1afd75596bf5
T. Winters <info@thomaswinters.be>
2018-05-05 21:21:29 +0200

    Updated rules about what tweets to reply to

--- src/main/java/be/thomaswinters/samson/alberto/AlbertoBotBuilder.java
+++ src/main/java/be/thomaswinters/samson/alberto/AlbertoBotBuilder.java
@@ -4,27 +4,41 @@ import be.thomaswinters.textgeneration.domain.generators.ITextGenerator;
 import be.thomaswinters.textgeneration.domain.parsers.DeclarationsFileParser;
 import be.thomaswinters.twitter.GeneratorTwitterBot;
 import be.thomaswinters.twitter.bot.TwitterBot;
-import be.thomaswinters.twitter.bot.util.TwitterLoginUtil;
-import be.thomaswinters.twitter.util.retriever.TwitterHomeRetriever;
+import be.thomaswinters.twitter.tweetsfetcher.TimelineTweetsFetcher;
+import be.thomaswinters.twitter.tweetsfetcher.filter.AlreadyParticipatedFilter;
+import be.thomaswinters.twitter.tweetsfetcher.filter.NotFollowingCurrentUserFilter;
+import be.thomaswinters.twitter.util.TwitterLogin;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import static be.thomaswinters.twitter.exception.TwitterUnchecker.uncheck;
+
 public class AlbertoBotBuilder {
     private final URL templateFile = ClassLoader.getSystemResource("templates/alberto-honger.decl");
 
-    public TwitterBot build() throws IOException {
+    public TwitterBot build() throws IOException, TwitterException {
         ITextGenerator generator = DeclarationsFileParser.createTemplatedGenerator(templateFile, new ArrayList<>());
+        Twitter twitter = TwitterLogin.getTwitterFromEnvironment();
 
-
-        Twitter twitter = TwitterLoginUtil.getTwitterFromEnvironment();
+        NotFollowingCurrentUserFilter followingChecker = new NotFollowingCurrentUserFilter(twitter, true);
 
         return new GeneratorTwitterBot(twitter,
                 Optional::empty,
                 new AlbertoBot(generator),
-                TwitterBot.MENTIONS_RETRIEVER, TwitterHomeRetriever::new);
+                twit ->
+                        TwitterBot.MENTIONS_RETRIEVER
+                                .apply(twit)
+                                .combineWith(
+                                        new TimelineTweetsFetcher(twit)
+                                                .filterOutOwnTweets(twit)
+                                                .filter(followingChecker)
+                                )
+                                .filter(uncheck(AlreadyParticipatedFilter::new, twitter))
+                                .filterOutRetweets());
     }
 }
